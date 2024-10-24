91c2ed5db0482404b79d29cc1872d0cc8bccda04
sHawkinss <lephuc156987@gmail.com>
2024-10-24 23:11:28 +0700

    1

---
+++ BellMan/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+class FileLineReader
+{
+    public static void Main()
+    {
+        string filePath = @"D:\INPUT.TXT";
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File does not exist :{0} ", filePath);
+            return;
+        }
+
+        string[] textFromFile = File.ReadAllLines(filePath);
+        foreach (string line in textFromFile)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
